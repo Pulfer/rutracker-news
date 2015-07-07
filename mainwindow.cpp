ffa0634fda702ca7ed01e741ef3ba6b938f1c124
Andrey Bondrov <andrey.bondrov@rosalab.ru>
2015-07-08 01:35:00 +1000

    Bump version to 0.4

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,7 +2,7 @@
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
-    setWindowTitle("Rutracker.org news 0.3");
+    setWindowTitle("Rutracker.org news 0.4");
     setMinimumSize(800, 600);
     basicScreen = new QWidget;
     setCentralWidget(basicScreen);
