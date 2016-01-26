f87fabd5bb1e56d720c2677d25d4efcbd81fba49
Andrey Bondrov <andrey.bondrov@rosalab.ru>
2016-01-27 01:29:16 +1000

    Drop unused code

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("Windows-1251"));
-//    QApplication::setStyle(new QWindowsStyle);
     MainWindow w;
     w.showMaximized();
     return a.exec();
